59bb1af1ebf05f610375a724ac6b23092c145ec9
XJuanCarlosXD <juancarlos192003@gmail.com>
2025-09-09 23:58:05 -0400

    master

--- track_1/src/main.tsx
+++ track_1/src/main.tsx
@@ -4,6 +4,19 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
+// Log de IP pública de Netlify (si está disponible)
+try {
+  const fnUrl = '/.netlify/functions/egress-ip'
+  fetch(fnUrl)
+    .then((r) => r.ok ? r.json() : null)
+    .then((j) => {
+      if (j && j.ip) {
+        console.log('[Netlify egress IP]', j.ip, 'region:', j.region)
+      }
+    })
+    .catch(() => { })
+} catch { }
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
